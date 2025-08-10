bb672f0bb5a37f0605ca5215830c5fd6bfcea260
Nathaniel Friday <udoakangn@gmail.com>
2025-08-10 18:16:11 +0100

    completed the project

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDice1 = "dice" + randomNumber1 + ".png"; 
+
+var randomImage1 = "image/" + randomDice1;    
+
+var image1 = document.querySelector(".img1");
+image1.setAttribute("src", randomImage1);
+
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; // 1-6
+
+var randomDice2 = "dice" + randomNumber2 + ".png";
+
+var randomImage2 = "image/" + randomDice2;
+
+var image2 = document.querySelector(".img2");
+image2.setAttribute("src", randomImage2);
+
+
+if (randomNumber1 > randomNumber2){
+      document.querySelector("h1").innerHTML = "Player 1 Wins!!!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!!";
+} else{
+    document.querySelector("h1").innerHTML = "Draw!!!";
+}
